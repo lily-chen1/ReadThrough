6fbf9814acf267dc0f2ef89419128708d85e945d
tinie doan <tido3408@colorado.edu>
2022-10-25 16:17:21 -0600

    navbar(incomplete)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,30 @@
 import "./resources/Home.css";
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"; 
+import NavBar from "./NavBar";
+import './resources/NavBar.css';
+
+
 function Home() {
   return (
     <div>
-      <h2>ReadThrough</h2>
-      <Link style={{ display: "block" }} to="/setuser">
-        Set user
-      </Link>
-      <Link style={{ display: "block" }} to="/users">
-        Users
-      </Link>
-      <Link style={{ display: "block" }} to="/profile">
-        Profile
-      </Link>
+      <NavBar />
     </div>
+
+    // <div>
+    //   <h2>ReadThrough</h2>
+    //   <Link style={{ display: "block" }} to="/setuser">
+    //     Set user
+    //   </Link>
+    //   <Link style={{ display: "block" }} to="/users">
+    //     Users
+    //   </Link>
+    //   <Link style={{ display: "block" }} to="/profile">
+    //     Profile
+    //   </Link>
+    //   <Link style={{ display: "block" }} to="/SearchResults">
+    //     Search
+    //   </Link>
+    // </div>
   );
 }
 
